4e34e8db384802ad78b6346ca3842842a2a779c0
SilverhawkQ <sadatahmed1996@gmail.com>
2018-12-01 14:45:30 -0500

    ..

---
+++ FinalProject/src/FinalProject/Main.java
@@ -0,0 +1,23 @@
+package FinalProject;
+
+import chrriis.dj.nativeswing.swtimpl.NativeInterface;
+import java.sql.SQLException;
+
+public class Main {
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+
+//        Model m = new Model();
+
+        
+        View v = new View();
+        
+        v.setVisible(true);
+        
+        
+//        m.setupDatabase();
+//        
+//        
+//        new Controller().init(v, m);
+    }
+}
