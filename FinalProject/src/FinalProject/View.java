d113b15624345338428f91585cb4a0a8a683e230
SilverhawkQ <sadatahmed1996@gmail.com>
2018-12-01 17:59:42 -0500

    ....

--- FinalProject/src/FinalProject/View.java
+++ FinalProject/src/FinalProject/View.java
@@ -4,8 +4,13 @@ package FinalProject;
 import chrriis.dj.nativeswing.swtimpl.NativeInterface;
 import chrriis.dj.nativeswing.swtimpl.components.JWebBrowser;
 import java.awt.BorderLayout;
+import java.awt.Image;
+import java.awt.event.ActionListener;
+import javax.swing.JPanel;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
+import javax.swing.tree.DefaultMutableTreeNode;
+import javax.swing.tree.DefaultTreeModel;
 
 
 public class View extends javax.swing.JFrame {
@@ -43,6 +48,11 @@ public class View extends javax.swing.JFrame {
         image = new javax.swing.JLabel();
         videoPanel = new javax.swing.JPanel();
         jPanel5 = new javax.swing.JPanel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        nameTree = new javax.swing.JTree();
+        selectField = new javax.swing.JTextField();
+        jLabel2 = new javax.swing.JLabel();
+        selectButton = new javax.swing.JButton();
         jPanel2 = new javax.swing.JPanel();
         jPanel3 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
@@ -59,6 +69,14 @@ public class View extends javax.swing.JFrame {
         descLabel.setText("Description");
 
         debutLabel.setText("Debut");
+
+        nameField.setFocusable(false);
+
+        universeField.setFocusable(false);
+
+        debutField.setFocusable(false);
+
+        descField.setFocusable(false);
 
         javax.swing.GroupLayout jPanel6Layout = new javax.swing.GroupLayout(jPanel6);
         jPanel6.setLayout(jPanel6Layout);
@@ -142,12 +160,38 @@ public class View extends javax.swing.JFrame {
             .addGap(0, 0, Short.MAX_VALUE)
         );
 
+        nameTree.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                nameTreeMouseClicked(evt);
+            }
+        });
+        jScrollPane1.setViewportView(nameTree);
+
+        jLabel2.setText("Character");
+
+        selectButton.setText("Select");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(316, 316, 316)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 288, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(18, 18, 18))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                                .addComponent(jLabel2)
+                                .addGap(18, 18, 18)
+                                .addComponent(selectField, javax.swing.GroupLayout.PREFERRED_SIZE, 165, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(54, 54, 54))))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGap(119, 119, 119)
+                        .addComponent(selectButton)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)))
                 .addComponent(jScrollPane2, javax.swing.GroupLayout.PREFERRED_SIZE, 317, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -164,7 +208,15 @@ public class View extends javax.swing.JFrame {
                         .addComponent(videoPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(18, 18, 18)
                         .addComponent(jPanel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                    .addComponent(jScrollPane2))
+                    .addComponent(jScrollPane2)
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 424, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(31, 31, 31)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(selectField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel2))
+                        .addGap(18, 18, 18)
+                        .addComponent(selectButton)))
                 .addContainerGap(20, Short.MAX_VALUE))
         );
 
@@ -216,22 +268,38 @@ public class View extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void nameTreeMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_nameTreeMouseClicked
+        DefaultMutableTreeNode selectedNode = (DefaultMutableTreeNode) nameTree.getSelectionPath().getLastPathComponent();
+        selectField.setText(selectedNode.getUserObject().toString());
+    }//GEN-LAST:event_nameTreeMouseClicked
+
     //-------------------------------------------------------------
 
-    public void setCharacterModel(TableModel m) {
-        nameTable.setModel(m);
+    
+    public void selectButtonListener(ActionListener a) {
+        selectButton.addActionListener(a);
+    }
+
+     public void setTableModel(DefaultTreeModel n) {
+        nameTree.setModel(n);
     }
     
+    
+    
+    
+    
     public void setVideo(String url) {
+        System.out.println("ssss");
         NativeInterface.open();
         
         
+        JPanel videoPanel = new JPanel();
         JWebBrowser wb = new JWebBrowser();
         videoPanel.add(wb, BorderLayout.CENTER);
         wb.setBarsVisible(false);
         wb.navigate(url);
         
-        NativeInterface.runEventPump();
+         NativeInterface.runEventPump();
         
         Runtime.getRuntime().addShutdownHook(new Thread(new Runnable(){
             @Override
@@ -240,8 +308,16 @@ public class View extends javax.swing.JFrame {
             }
         }));
         
+       
+
     }
     
+
+    
+    
+    public String getName(){
+        return selectField.getText();
+    }
     
     public void setName(String name){
         nameField.setText(name);
@@ -260,6 +336,7 @@ public class View extends javax.swing.JFrame {
     }
     
     public void setImage(String dir){
+        
         image.setIcon(new javax.swing.ImageIcon(dir));
         //image.setIcon(new javax.swing.ImageIcon("D:\\sasdas.png"));
     }
@@ -276,16 +353,21 @@ public class View extends javax.swing.JFrame {
     private javax.swing.JLabel descLabel;
     private javax.swing.JLabel image;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel4;
     private javax.swing.JPanel jPanel5;
     private javax.swing.JPanel jPanel6;
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTabbedPane jTabbedPane1;
     private javax.swing.JTextField nameField;
     private javax.swing.JLabel nameLabel;
+    private javax.swing.JTree nameTree;
+    private javax.swing.JButton selectButton;
+    private javax.swing.JTextField selectField;
     private javax.swing.JTextField universeField;
     private javax.swing.JLabel unverseLabel;
     private javax.swing.JPanel videoPanel;
